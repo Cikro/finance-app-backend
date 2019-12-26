227fa78e237500a6c040b1f3abbb84926ea8d049
Niko <niko@localhost.localdomain>
2019-12-26 00:56:48 -0500

    By default, prevent EF from creating IDs since MariaDB does this

--- finance-app/Types/EFModels/DatabaseObject.cs
+++ finance-app/Types/EFModels/DatabaseObject.cs
@@ -1,9 +1,11 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace finance_app.Types.EFModels
 {
     public class DatabaseObject
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public uint Id { get; set; }
 
         public DateTime DateCreated { get; set; }
