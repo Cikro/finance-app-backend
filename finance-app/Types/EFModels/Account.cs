de5d9d57145b4153945228ecd7829920d1918203
Niko <niko@localhost.localdomain>
2019-12-30 00:41:33 -0500

    Specifying table for account EF model.

--- finance-app/Types/EFModels/Account.cs
+++ finance-app/Types/EFModels/Account.cs
@@ -1,12 +1,16 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace finance_app.Types.EFModels
 {
+    [Table("Accounts")]
     public class Account : DatabaseObject
     {
         [Required]
         public string Name { get; set; }
+        [Required]
+        public uint UserId { get; set; }
         public string Description { get; set; }
         public double Balance { get; set; }
         [Required]
